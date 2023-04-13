e1e556180fe4d1448ea008e90bc640e14e95a6ff
tmaksimenko <timothymaksimenko@gmail.com>
2023-04-13 12:21:41 -0400

    log for app started

--- src/main/java/com/tmaksimenko/storefront/StorefrontApplication.java
+++ src/main/java/com/tmaksimenko/storefront/StorefrontApplication.java
@@ -1,13 +1,20 @@
 package com.tmaksimenko.storefront;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class StorefrontApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(StorefrontApplication.class, args);
+
+		log.info("App Started!");
 	}
 
 }
