58c0079b0203f0ccaab8b120f70890b5c06d6b1f
tmaksimenko <timothymaksimenko@gmail.com>
2023-04-13 11:50:53 -0400

    Initial commit for project files

---
+++ src/test/java/com/itcsystems/storefront/StorefrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcsystems.storefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorefrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
