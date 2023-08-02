0211c203fc2902efc7ee4932e9c118c4400ec854
tmaksimenko <timothymaksimenko@gmail.com>
2023-08-02 15:26:26 -0400

    added Hibernate annotations to ensure orphan removal (possibly redundant)

--- common/src/main/java/com/tmaksimenko/storefront/model/account/Cart.java
+++ common/src/main/java/com/tmaksimenko/storefront/model/account/Cart.java
@@ -3,6 +3,8 @@ package com.tmaksimenko.storefront.model.account;
 import com.tmaksimenko.storefront.model.payment.Payment;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.Map;
 
@@ -20,11 +22,13 @@ public class Cart {
 
 
     @SuppressWarnings("JpaDataSourceORMInspection") // refuses to see columns
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "cart",
             joinColumns = {@JoinColumn(name = "account_id", referencedColumnName = "id")})
     @MapKeyColumn(name = "product_id")
     @Column(name = "quantity")
+    @JoinColumn(name = "cart_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     Map<Long, Integer> items;
 
 }
