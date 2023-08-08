58ea1ac80eea0b8a1ac42fc4cdc911d2330fc362
tmaksimenko <timothymaksimenko@gmail.com>
2023-08-08 14:44:38 -0400

    refactoring and minor fixes

--- api/src/main/java/com/tmaksimenko/storefront/controller/user/ProductController.java
+++ api/src/main/java/com/tmaksimenko/storefront/controller/user/ProductController.java
@@ -1,12 +1,13 @@
 package com.tmaksimenko.storefront.controller.user;
 
 import com.tmaksimenko.storefront.annotation.ExcludeFromJacocoGeneratedReport;
-import com.tmaksimenko.storefront.dto.product.ProductDto;
 import com.tmaksimenko.storefront.dto.order.CartDto;
+import com.tmaksimenko.storefront.dto.product.ProductDto;
 import com.tmaksimenko.storefront.exception.AccountNotFoundException;
+import com.tmaksimenko.storefront.exception.ProductNotFoundException;
+import com.tmaksimenko.storefront.model.Product;
 import com.tmaksimenko.storefront.model.account.Account;
 import com.tmaksimenko.storefront.model.account.Cart;
-import com.tmaksimenko.storefront.model.Product;
 import com.tmaksimenko.storefront.service.account.AccountService;
 import com.tmaksimenko.storefront.service.product.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -24,10 +25,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
-
-import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 
 @Tag(name = "User Operations")
 @RestController
@@ -52,7 +53,9 @@ public class ProductController {
     @Operation(summary = "Fetch individual product")
     @GetMapping("/view")
     public ResponseEntity<Product> viewProduct (@RequestParam Long id) {
-        return ResponseEntity.of(productService.findById(id));
+        return new ResponseEntity<>(productService.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "PRODUCT NOT FOUND", new ProductNotFoundException())),
+                HttpStatus.OK);
     }
 
     @Operation(summary = "View cart", parameters =
@@ -65,7 +68,7 @@ public class ProductController {
     public ResponseEntity<Cart> viewCart () {
         return new ResponseEntity<>(
                 accountService.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName())
-                        .orElseThrow(AccountNotFoundException::new).getCart(),
+                        .get().getCart(),
                 HttpStatus.OK);
     }
 
@@ -79,7 +82,10 @@ public class ProductController {
     public ResponseEntity<String> createCart (@RequestBody CartDto cartDto) {
         Account account = accountService.findByUsername(SecurityContextHolder.getContext().getAuthentication()
                 .getName()).orElseThrow(AccountNotFoundException::new);
-        if (!isEmpty(account.getCart()))
+        if (!account.getCart().equals(Cart.builder()
+                .payment(null)
+                .price(null)
+                .items(new HashMap<>()).build()))
             deleteCart();
         account.setCart(productService.createCart(cartDto));
         return new ResponseEntity<>("CART CREATED", HttpStatus.CREATED);
@@ -94,7 +100,10 @@ public class ProductController {
     @DeleteMapping("/cart")
     public ResponseEntity<String> deleteCart () {
         Account account = accountService.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName()).get();
-        if (isEmpty(account.getCart())) {
+        if (account.getCart().equals(Cart.builder()
+                .payment(null)
+                .price(null)
+                .items(new HashMap<>()).build())) {
             return new ResponseEntity<>("NO CART FOUND", HttpStatus.OK);
         } else {
             account.setCart(new Cart());
